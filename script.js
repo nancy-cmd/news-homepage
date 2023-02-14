7c9293503973c54b28d86cd22c7d3781e979ad4f
Nancy Chisenga <nanciechisenga@gmail.com>
2023-02-14 20:46:37 +0000

    created a function meant to hide the menu list on mobile devices

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 function hamburgerMenu() {
-    
+    let iconBars = document.getElementById("mainNav");
+    if (iconBars.className === "topnav"){
+        iconBars.className += " responsive";
+    } else {
+        iconBars.className = "topnav";
+    }
 }
